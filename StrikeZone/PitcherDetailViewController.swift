e2cdf571ab95b6cd52a2cd6dac228312cd79e97b
Patrick Landin <patricklandin@gmail.com>
2015-01-26 09:19:30 -0800

    Created 3 VC files

---
+++ StrikeZone/PitcherDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PitcherDetailViewController.swift
+//  StrikeZone
+//
+//  Created by Patrick Landin on 1/26/15.
+//  Copyright (c) 2015 QadburyDreams. All rights reserved.
+//
+
+import UIKit
+
+class PitcherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ StrikeZone/StrikeZoneViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StrikeZoneViewController.swift
+//  StrikeZone
+//
+//  Created by Patrick Landin on 1/26/15.
+//  Copyright (c) 2015 QadburyDreams. All rights reserved.
+//
+
+import UIKit
+
+class StrikeZoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
