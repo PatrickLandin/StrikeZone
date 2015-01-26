643bc924fab09458c2b8bc99d1ff137aaea43359
Patrick Landin <patricklandin@gmail.com>
2015-01-26 10:25:49 -0800

    created Pitcher and HeatMap models

---
+++ StrikeZone/HeatMap.swift
@@ -0,0 +1,19 @@
+//
+//  HeatMap.swift
+//  StrikeZone
+//
+//  Created by Patrick Landin on 1/26/15.
+//  Copyright (c) 2015 QadburyDreams. All rights reserved.
+//
+
+import UIKit
+
+class HeatMap {
+  var pitchCount : Int
+  var date : String
+  
+  init () {
+    self.pitchCount = 0
+    self.date = ""
+  }
+}
--- StrikeZone/PitcherMenuViewController.swift
+++ StrikeZone/PitcherMenuViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class PitcherMenuViewController: UIViewController {
+class PitcherMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var tableView: UITableView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      self.tableView.delegate = self
+      self.tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +25,23 @@ class PitcherMenuViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  @IBAction func addButtonPressed(sender: AnyObject) {
+    
+  }
+  
+  
+  @IBAction func deleteButtonPressed(sender: AnyObject) {
+    
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    return 10
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let cell = tableView.dequeueReusableCellWithIdentifier("CELL", forIndexPath: indexPath) as UITableViewCell
+    return cell
+  }
     
 
 }
